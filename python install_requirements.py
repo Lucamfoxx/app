a5e68dd89d55c0ed31722791126a0c6f4d9d06d9
Lucamfoxx <guilhermedesc@outlook.com>
2023-11-29 17:33:41 -0300

    29/11/23/2

--- python install_requirements.py
+++ python install_requirements.py
@@ -1,15 +1,20 @@
 import subprocess
 
-# Lista de pacotes necessários
-required_packages = [
-    'requests',
-    'matplotlib',
-    'kivy',
-    'kivymd',
-]
 
-# Instalação dos pacotes
-for package in required_packages:
-    subprocess.run(['pip', 'install', package])
+required_packages = {
+    'requests': '2.31.0',
+    'matplotlib': '3.8.2',
+    'kivy': '2.2.1',
+    'kivymd': '1.1.1',
+}
 
-print("Todos os pacotes foram instalados com sucesso.")
+try:
+    # Instalação dos pacotes com versões específicas
+    for package, version in required_packages.items():
+        subprocess.run(['pip', 'install', f'{package}=={version}'])
+        
+    # Exibir versões após a instalação
+    subprocess.run(['pip', 'freeze'])
+
+except Exception as e:
+    print(f"Ocorreu um erro durante a instalação: {e}")
